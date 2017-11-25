af45d18cfc694647a8a0d81507013049fdc1adb2
flawnn <baran.peters@lessing-ffm.net>
2017-11-25 21:35:30 +0100

    Fast Final

--- popup.js
+++ popup.js
@@ -2,7 +2,7 @@ var pro = 0;
 var con = 0;
 var sum = 0;
 var tablink;
-var stat = document.cookie;
+var cook = document.cookie;
 chrome.tabs.getSelected(null,function(tab) {
     tablink = tab.url;
 });
@@ -15,6 +15,13 @@ function formatParams( params ){
         })
         .join("&")
 }
+  function getCookie(name)
+  {
+    var re = new RegExp(name + "=([^;]+)");
+    var value = re.exec(document.cookie);
+    return (value != null) ? value[1] : null;
+  }
+
 
 function getStats(){
         var request = new XMLHttpRequest();
@@ -43,40 +50,37 @@ function getStats(){
 
 function sendVote(vote, Url){
         var request = new XMLHttpRequest();
-
-        request.open('GET', 'http://10.23.41.199:8081/vote' + formatParams({link: Url, vote: vote}), false);
-
+        request.open('GET', 'http://10.23.41.199:8081/vote' + formatParams({link: Url, vote: vote}), true);
         request.onload = function() {
           if (request.status >= 200 && request.status < 400) {
-            var data = JSON.parse(request.responseText);
-            console.log(data.Message);
           } else {
             // We reached our target server, but it returned an error
-
           }
         };
 
         request.onerror = function() {
           // There was a connection error of some sort
         };
-
         request.send();
 
   }
 
 
 function bestaetigen() {
-   
+        if(!(getCookie("lastPage") === tablink)){
+            document.cookie = "status=unmarked";
+            document.cookie = "lastPage=" + tablink;
+        }
         var stats = getStats();
-        console.log(stats.upv);
         setTimeout(function(){
-        if (!(stat === "status=marked")) {
+        if (!(getCookie("status") === "marked")) {
+
         sendVote(1, tablink);
         sumpro = "Positiv: " + Math.round(stats.upv/(stats.upv + stats.dow) * 100) + "%";
         document.getElementById("sumpro").innerHTML = sumpro;
         sumcon = "Negativ: " + Math.round(stats.dow/(stats.upv + stats.dow) * 100) + "%";
         document.getElementById("sumcon").innerHTML = sumcon;
-        document.cookie = "status = marked";
+        document.cookie = "status = marked;"
         document.getElementById("confirmButton").setAttribute("disabled", "disabled");
         document.getElementById("reportButton").setAttribute("disabled", "disabled");
     } else {
@@ -94,15 +98,20 @@ function bestaetigen() {
 }
 
 function report() {
+    if(!(getCookie("lastPage") === tablink)){
+            document.cookie = "status=unmarked";
+            document.cookie = "lastPage=" + tablink;
+            cook = document.cookie;
+        }
         var stats = getStats();
         setTimeout(function(){
-        if (!(stat === "status=marked")) {
+        if (!(getCookie("status") === "marked")) {
         sendVote(0, tablink);
         sumpro = "Positiv: " + Math.round(stats.upv/(stats.upv + stats.dow) * 100) + "%";
         document.getElementById("sumpro").innerHTML = sumpro;
         sumcon = "Negativ: " + Math.round(stats.dow/(stats.upv + stats.dow) * 100) + "%";
         document.getElementById("sumcon").innerHTML = sumcon;
-        document.cookie = "status = marked";
+        document.cookie = "status = marked;"
         document.getElementById("confirmButton").setAttribute("disabled", "disabled");
         document.getElementById("reportButton").setAttribute("disabled", "disabled");
     } else {
@@ -119,7 +128,6 @@ function report() {
 
 document.addEventListener('DOMContentLoaded', function () {
   document.querySelector('#CookieButton').addEventListener('click', function () {
-        document.cookie = "status = not marked";
   });
 
 });
