0a008aeefce08d4e0047f088e948ad980619cbc6
G3bE <32980656+G3bE@users.noreply.github.com>
2017-11-25 13:19:59 +0100

    Add files via upload

--- popup.js
+++ popup.js
@@ -1,18 +1,34 @@
-var pro = 15;
-var con = 4;
+var pro = 0;
+var con = 0;
 var sum = 0;
+var stat = document.cookie;
+
+document.cookie = "status = not marked";
 
 function bestaetigen() {
-    pro++;
-    sum = Math.round(pro/(pro+con)*100) + '%';
-    document.getElementById("sum").innerHTML = sum;
+    if (!(stat === "status=marked")) {
+        pro++;
+        sumpro = Math.round(pro/(pro + con) * 100) + "%";
+        document.getElementById("sumpro").innerHTML = sumpro;
+        sumcon = Math.round(con/(pro + con) * 100) + "%";
+        document.getElementById("sumcon").innerHTML = sumcon;
+        document.cookie = "status = marked";
+        stat = document.cookie
+    } else {
+        document.write("Du hast diese/n News/Post schon bewertet!");
+    }
 }
+
 function report() {
-    con++;    
-    sum = Math.round(pro/(pro+con)*100) + '%';
-    document.getElementById("sum").innerHTML = sum;
+    if (!(stat === "status=marked")) {
+        con++;
+        sumpro = Math.round(pro/(pro + con) * 100) + "%";
+        document.getElementById("sumpro").innerHTML = sumpro;
+        sumcon = Math.round(con/(pro + con) * 100) + "%";
+        document.getElementById("sumcon").innerHTML = sumcon;
+        document.cookie = "status = marked";
+        stat = document.cookie
+    } else {
+        document.write("Du hast diese/n News/Post schon bewertet!");
+    }
 }
-
-sum = Math.round(pro/(pro+con)*100) + '%';
-//document.getElementById("sum").innerHTML = sum;
-
